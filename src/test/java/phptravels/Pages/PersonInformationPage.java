af06c22350755eaa7138be852b1d5263d5799233
sylwiaromaniewicz <42293937+sylwiaromaniewicz@users.noreply.github.com>
2018-10-05 09:27:41 +0200

    something done at work :)

--- src/test/java/phptravels/Pages/PersonInformationPage.java
+++ src/test/java/phptravels/Pages/PersonInformationPage.java
@@ -1,6 +1,7 @@
 package phptravels.Pages;
 
 import aCommon.BasePage;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,16 +17,42 @@ public class PersonInformationPage extends BasePage {
     @FindBy (name="email")
     private WebElement emailTextBox;
 
+    @FindBy (name="confirmemail")
+    private WebElement confirmEmailTexTBox;
+
     @FindBy (name="phone")
     private WebElement phoneTextBox;
+
+    @FindBy (name="address")
+    private WebElement addressTextBox;
+
+    @FindBy (xpath="/html/body/div[4]/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[1]/form/div[6]/div[2]/div/a")
+    private WebElement countryClickOnBox;
 
     @FindBy (xpath="/html/body/div[10]/div/input")
     private WebElement countryTextBox;
 
-    @FindBy (xpath="/html/body/div[4]/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[1]/form/div[8]/div/div[1]/label/div")
-    private WebElement checkBox;
+    @FindBy (name="guest")
+    private WebElement guestSubbmitButton;
 
+    public void iFillInTheForm (String firstname, String lastname, String email, String phone, String address, String country){
+        firstNameTextBox.sendKeys(firstname);
+        lastNameTextBox.sendKeys(lastname);
+        emailTextBox.sendKeys(email);
+        confirmEmailTexTBox.sendKeys(email);
+        phoneTextBox.sendKeys(phone);
+        addressTextBox.sendKeys(address);
+        countryClickOnBox.click();
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        countryTextBox.sendKeys(country);
+        countryTextBox.sendKeys(Keys.RETURN);
+        guestSubbmitButton.click();
 
+    }
 
     public PersonInformationPage(WebDriver driver) {
         super(driver);
